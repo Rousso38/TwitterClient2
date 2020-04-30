334071c1c41aadb05df0a1a9c7e72560138f4bc8
Rousso38 <zidorjeanrousso@gmail.com>
2020-04-29 17:27:13 -0700

    Initial commit

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/SampleModelDao.java
@@ -0,0 +1,21 @@
+package com.codepath.apps.restclienttemplate.models;
+
+import androidx.room.Dao;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface SampleModelDao {
+
+    // @Query annotation requires knowing SQL syntax
+    // See http://www.sqltutorial.org/
+    
+    @Query("SELECT * FROM SampleModel WHERE id = :id")
+    SampleModel byId(long id);
+
+    @Query("SELECT * FROM SampleModel ORDER BY ID DESC LIMIT 300")
+    List<SampleModel> recentItems();
+
+
+}
